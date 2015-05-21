020cbef189210cfb1cc8a155ab61e8c4f37ced9f
OpenUI5 <openui5@sap.com>
2015-05-21 11:22:29 +0200

    1.28.7

--- resources/sap/ui/unified/calendar/Month.js
+++ resources/sap/ui/unified/calendar/Month.js
@@ -27,7 +27,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/LocaleDa
 	 * If used inside the calendar the properties and aggregation are directly taken from the parent
 	 * (To not duplicate and sync DateRanges and so on...)
 	 * @extends sap.ui.core.Control
-	 * @version 1.28.6
+	 * @version 1.28.7
 	 *
 	 * @constructor
 	 * @public
