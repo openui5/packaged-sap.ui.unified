8fc5c515b02cd1876050a07e92d51c817b254deb
OpenUI5 <openui5@sap.com>
2015-07-27 14:17:04 +0200

    1.30.4

--- resources/sap/ui/unified/MenuRenderer.js
+++ resources/sap/ui/unified/MenuRenderer.js
@@ -15,7 +15,7 @@ sap.ui.define(['jquery.sap.global'],
 	 * Menu renderer.
 	 * @author SAP - TD Core UI&AM UI Infra
 	 *
-	 * @version 1.30.3
+	 * @version 1.30.4
 	 * @namespace
 	 */
 	var MenuRenderer = {
