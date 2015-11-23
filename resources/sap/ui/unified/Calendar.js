6f90862de66c14782f9ac890b0e9daa74868fdfc
OpenUI5 <openui5@sap.com>
2015-11-23 13:20:59 +0100

    1.32.7

--- resources/sap/ui/unified/Calendar.js
+++ resources/sap/ui/unified/Calendar.js
@@ -25,7 +25,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/LocaleDa
 	 * Basic Calendar.
 	 * This calendar is used for DatePickers
 	 * @extends sap.ui.core.Control
-	 * @version 1.32.6
+	 * @version 1.32.7
 	 *
 	 * @constructor
 	 * @public
@@ -1222,9 +1222,17 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/LocaleDa
 
 			var oDate = CalendarUtils._createUniversalUTCDate(oEvent.getParameter("date"));
 			var bOtherMonth = oEvent.getParameter("otherMonth");
+			var bRestoreOldDate = oEvent.getParameter("restoreOldDate");
 			var that = this;
 
-			_focusDate(that, oDate, bOtherMonth);
+			if (bRestoreOldDate) {
+				// in multimonth mode stay at the last focused date
+				if (!jQuery.sap.equal(this._getFocusedDate(), oDate)) {
+					_renderMonth(that, false);
+				}
+			} else {
+				_focusDate(that, oDate, bOtherMonth);
+			}
 
 		}
 
