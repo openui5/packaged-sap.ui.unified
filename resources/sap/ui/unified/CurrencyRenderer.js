b9326d3ea3a0e8e50c7510384149d1524130529f
OpenUI5 <openui5@sap.com>
2016-02-11 11:16:33 +0100

    1.34.6

--- resources/sap/ui/unified/CurrencyRenderer.js
+++ resources/sap/ui/unified/CurrencyRenderer.js
@@ -14,7 +14,7 @@ sap.ui.define(['jquery.sap.global'],
 	/**
 	 * Currency renderer.
 	 *
-	 * @version 1.34.5
+	 * @version 1.34.6
 	 * @namespace
 	 */
 	var CurrencyRenderer = {
--- resources/sap/ui/unified/Shell.js
+++ resources/sap/ui/unified/Shell.js
@@ -25,7 +25,7 @@ sap.ui.define(['jquery.sap.global', './ShellHeader', './ShellLayout', './library
 	 * @extends sap.ui.unified.ShellLayout
 	 *
 	 * @author SAP SE
-	 * @version 1.34.5
+	 * @version 1.34.6
 	 *
 	 * @constructor
 	 * @public
