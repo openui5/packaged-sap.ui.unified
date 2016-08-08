e1ccfbf15cda776ee04554e3467bef1ffa243833
OpenUI5 <openui5@sap.com>
2016-08-08 20:21:27 +0200

    1.40.4

--- resources/sap/ui/unified/Shell.js
+++ resources/sap/ui/unified/Shell.js
@@ -25,7 +25,7 @@ sap.ui.define(['jquery.sap.global', './ShellHeader', './ShellLayout', './library
 	 * @extends sap.ui.unified.ShellLayout
 	 *
 	 * @author SAP SE
-	 * @version 1.40.3
+	 * @version 1.40.4
 	 *
 	 * @constructor
 	 * @public
