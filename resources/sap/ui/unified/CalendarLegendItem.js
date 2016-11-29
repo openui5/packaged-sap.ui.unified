e5672730b0717212558dac46cf80357630699c77
OpenUI5 <openui5@sap.com>
2016-11-29 09:28:02 +0100

    1.40.12

--- resources/sap/ui/unified/CalendarLegendItem.js
+++ resources/sap/ui/unified/CalendarLegendItem.js
@@ -18,7 +18,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Element', './library'],
 	 * @class
 	 * Item to be displayed in a CalendarLegend.
 	 * @extends sap.ui.core.Element
-	 * @version 1.40.11
+	 * @version 1.40.12
 	 *
 	 * @constructor
 	 * @public
