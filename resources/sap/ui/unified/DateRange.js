55a96b7a5b3a38afc189096315cf8e5f3e66712f
OpenUI5 <openui5@sap.com>
2017-04-07 18:18:10 +0200

    1.40.18

--- resources/sap/ui/unified/DateRange.js
+++ resources/sap/ui/unified/DateRange.js
@@ -20,7 +20,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Element', './library'],
 	 * @class
 	 * Date range for use in DatePicker
 	 * @extends sap.ui.core.Element
-	 * @version 1.40.17
+	 * @version 1.40.18
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/unified/MenuItem.js
+++ resources/sap/ui/unified/MenuItem.js
@@ -23,7 +23,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/IconPool', './MenuItemBase', '.
 	 * @extends sap.ui.unified.MenuItemBase
 	 *
 	 * @author SAP SE
-	 * @version 1.40.17
+	 * @version 1.40.18
 	 * @since 1.21.0
 	 *
 	 * @constructor
