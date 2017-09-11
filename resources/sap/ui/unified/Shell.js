c90da5733847fa94cba7ed60452958abace928e8
OpenUI5 <openui5@sap.com>
2017-09-11 20:23:46 +0200

    1.44.20

--- resources/sap/ui/unified/Shell.js
+++ resources/sap/ui/unified/Shell.js
@@ -25,7 +25,7 @@ sap.ui.define(['jquery.sap.global', './ShellHeader', './ShellLayout', './library
 	 * @extends sap.ui.unified.ShellLayout
 	 *
 	 * @author SAP SE
-	 * @version 1.44.19
+	 * @version 1.44.20
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/unified/ShellHeadItem.js
+++ resources/sap/ui/unified/ShellHeadItem.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Element', 'sap/ui/core/IconPool
 	 * @extends sap.ui.core.Element
 	 *
 	 * @author SAP SE
-	 * @version 1.44.19
+	 * @version 1.44.20
 	 *
 	 * @constructor
 	 * @public
