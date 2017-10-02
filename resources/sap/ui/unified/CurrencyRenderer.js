cab5584b23861ea13196b4baf28c416d45db2220
OpenUI5 <openui5@sap.com>
2017-10-02 17:01:20 +0200

    1.44.21

--- resources/sap/ui/unified/CurrencyRenderer.js
+++ resources/sap/ui/unified/CurrencyRenderer.js
@@ -14,7 +14,7 @@ sap.ui.define(['jquery.sap.global'],
 	/**
 	 * Currency renderer.
 	 *
-	 * @version 1.44.20
+	 * @version 1.44.21
 	 * @namespace
 	 */
 	var CurrencyRenderer = {
--- resources/sap/ui/unified/MenuItemBase.js
+++ resources/sap/ui/unified/MenuItemBase.js
@@ -23,7 +23,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Element', './library'],
 	 * @extends sap.ui.core.Element
 	 *
 	 * @author SAP SE
-	 * @version 1.44.20
+	 * @version 1.44.21
 	 * @since 1.21.0
 	 *
 	 * @constructor
