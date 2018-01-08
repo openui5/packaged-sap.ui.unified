970e1936d13e55f4a81bd26502b3642f08d11c88
OpenUI5 <openui5@sap.com>
2018-01-08 13:42:13 +0100

    1.48.17

--- resources/sap/ui/unified/CalendarRow.js
+++ resources/sap/ui/unified/CalendarRow.js
@@ -1,6 +1,6 @@
 /*!
  * UI development toolkit for HTML5 (OpenUI5)
- * (c) Copyright 2009-2017 SAP SE or an SAP affiliate company.
+ * (c) Copyright 2009-2018 SAP SE or an SAP affiliate company.
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
 
@@ -28,7 +28,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/Device', 'sap
 	 * @class
 	 * A calendar row with a header and appointments. The Appointments will be placed in the defined interval.
 	 * @extends sap.ui.core.Control
-	 * @version 1.48.16
+	 * @version 1.48.17
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/unified/CalendarWeekInterval.js
+++ resources/sap/ui/unified/CalendarWeekInterval.js
@@ -1,6 +1,6 @@
 /*!
  * UI development toolkit for HTML5 (OpenUI5)
- * (c) Copyright 2009-2017 SAP SE or an SAP affiliate company.
+ * (c) Copyright 2009-2018 SAP SE or an SAP affiliate company.
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
 
@@ -40,7 +40,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/unified/calendar/CalendarUtils', 'sa
 		 * Navigation via year picker switches to the beginning of the same week, but in the chosen year.
 		 *
 		 * @extends sap.ui.unified.CalendarDateInterval
-		 * @version 1.48.16
+		 * @version 1.48.17
 		 *
 		 * @constructor
 		 * @private
