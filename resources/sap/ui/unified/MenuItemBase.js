4af1b7a5c1a59d2a0450721454c551951163c085
OpenUI5 <openui5@sap.com>
2018-03-12 18:58:01 +0100

    1.44.29

--- resources/sap/ui/unified/MenuItemBase.js
+++ resources/sap/ui/unified/MenuItemBase.js
@@ -23,7 +23,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Element', './library'],
 	 * @extends sap.ui.core.Element
 	 *
 	 * @author SAP SE
-	 * @version 1.44.28
+	 * @version 1.44.29
 	 * @since 1.21.0
 	 *
 	 * @constructor
--- resources/sap/ui/unified/Shell.js
+++ resources/sap/ui/unified/Shell.js
@@ -25,7 +25,7 @@ sap.ui.define(['jquery.sap.global', './ShellHeader', './ShellLayout', './library
 	 * @extends sap.ui.unified.ShellLayout
 	 *
 	 * @author SAP SE
-	 * @version 1.44.28
+	 * @version 1.44.29
 	 *
 	 * @constructor
 	 * @public
