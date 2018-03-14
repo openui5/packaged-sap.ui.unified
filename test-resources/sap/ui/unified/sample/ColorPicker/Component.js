693a09775a2972eee8cdc655e23415769b67364f
Matthias Osswald <mat.osswald@sap.com>
2018-03-14 14:54:23 +0100

    1.54.0

---
+++ test-resources/sap/ui/unified/sample/ColorPicker/Component.js
@@ -0,0 +1,31 @@
+sap.ui.define(['sap/ui/core/UIComponent'],
+	function(UIComponent) {
+	"use strict";
+
+	var Component = UIComponent.extend("sap.ui.unified.sample.ColorPicker.Component", {
+
+		metadata : {
+			rootView : {
+				"viewName": "sap.ui.unified.sample.ColorPicker.View",
+				"type": "XML",
+				"async": true
+			},
+			dependencies : {
+				libs : [
+					"sap.ui.unified"
+				]
+			},
+			config : {
+				sample : {
+					stretch : true,
+					files : [
+						"View.view.xml"
+					]
+				}
+			}
+		}
+	});
+
+	return Component;
+
+});
