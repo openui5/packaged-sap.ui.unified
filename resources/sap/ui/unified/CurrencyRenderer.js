627472b516054a9138beedd2f8dd1492570da355
OpenUI5 <openui5@sap.com>
2018-05-16 19:55:32 +0200

    1.38.36

--- resources/sap/ui/unified/CurrencyRenderer.js
+++ resources/sap/ui/unified/CurrencyRenderer.js
@@ -14,7 +14,7 @@ sap.ui.define(['jquery.sap.global'],
 	/**
 	 * Currency renderer.
 	 *
-	 * @version 1.38.35
+	 * @version 1.38.36
 	 * @namespace
 	 */
 	var CurrencyRenderer = {
--- resources/sap/ui/unified/DateRange.js
+++ resources/sap/ui/unified/DateRange.js
@@ -20,7 +20,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Element', './library'],
 	 * @class
 	 * Date range for use in DatePicker
 	 * @extends sap.ui.core.Element
-	 * @version 1.38.35
+	 * @version 1.38.36
 	 *
 	 * @constructor
 	 * @public
