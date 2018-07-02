18d4020b68202f55f77ba4c48984673b52fc22cb
OpenUI5 <openui5@sap.com>
2018-07-02 19:05:13 +0200

    1.38.37

--- resources/sap/ui/unified/CurrencyRenderer.js
+++ resources/sap/ui/unified/CurrencyRenderer.js
@@ -14,7 +14,7 @@ sap.ui.define(['jquery.sap.global'],
 	/**
 	 * Currency renderer.
 	 *
-	 * @version 1.38.36
+	 * @version 1.38.37
 	 * @namespace
 	 */
 	var CurrencyRenderer = {
--- resources/sap/ui/unified/FileUploader.js
+++ resources/sap/ui/unified/FileUploader.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', './library', 'sap/ui/
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author SAP SE
-	 * @version 1.38.36
+	 * @version 1.38.37
 	 *
 	 * @constructor
 	 * @public
@@ -1169,6 +1169,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', './library', 'sap/ui/
 					iKeyCode != eKC.PAGE_UP &&
 					iKeyCode != eKC.PAGE_DOWN &&
 					iKeyCode != eKC.END &&
+					iKeyCode != eKC.ESCAPE &&
 					iKeyCode != eKC.HOME &&
 					iKeyCode != eKC.ARROW_LEFT &&
 					iKeyCode != eKC.ARROW_UP &&
@@ -1461,7 +1462,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', './library', 'sap/ui/
 					jQuery.sap.log.info("File uploaded to " + that.getUploadUrl());
 					var sResponse;
 					try {
-						sResponse = that.oIFrameRef.contentDocument.body.innerHTML;
+						sResponse = that.oIFrameRef.contentWindow.document.body.innerHTML;
 					} catch (ex) {
 						// in case of cross-domain submit we get a permission denied exception
 						// when we try to access the body of the IFrame document
