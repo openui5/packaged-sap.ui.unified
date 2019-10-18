2545c22deb37711cd3189aaff25098b4d0160ab0
OpenUI5 <openui5@sap.com>
2019-10-18 19:11:57 +0200

    1.44.43

--- resources/sap/ui/unified/MenuTextFieldItem.js
+++ resources/sap/ui/unified/MenuTextFieldItem.js
@@ -23,7 +23,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/ValueStateSupport', './MenuItem
 	 * @extends sap.ui.unified.MenuItemBase
 	 *
 	 * @author SAP SE
-	 * @version 1.44.42
+	 * @version 1.44.43
 	 * @since 1.21.0
 	 *
 	 * @constructor
